750e36abf4015b4f091b77d064718e4925c79615
Girum <70520544+girume1@users.noreply.github.com>
2025-12-19 17:53:59 +0300

    service.rs

--- contracts/src/service.rs
+++ contracts/src/service.rs
@@ -1,12 +1,14 @@
 use async_trait::async_trait;
 use linera_sdk::{base::WithServiceRuntime, Service, ViewStorageContext};
+use tic_tac_toe::{state::TicTacToeState, Query};
 
 #[async_trait]
 impl Service for TicTacToeState {
     type Error = String;
     type Storage = ViewStorageContext;
 
-    async fn handle_query(&self, _query: ()) -> Result<[Option<char>; 9], Self::Error> {
-        Ok(*self.board.get())
+    async fn handle_query(&self, _query: Query) -> Result<TicTacToeState, Self::Error> {
+        // Returns the entire state (board, next player, winner) to the frontend
+        Ok(self.clone())
     }
 }
