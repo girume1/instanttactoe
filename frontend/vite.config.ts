2140100ea25e6dd4fe4983922ea8df20052b3981
Girum <70520544+girume1@users.noreply.github.com>
2026-01-23 00:49:37 +0300

    Vite config
    Updated the Vite configuration to include external dependencies and optimize dependencies.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: "/",
+  base: '',
   server: {
     headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "credentialless",
-      "Cross-Origin-Resource-Policy": "cross-origin",
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'credentialless',
+      'Cross-Origin-Resource-Policy': 'cross-origin',
     },
   },
   build: {
     rollupOptions: {
-      input: path.resolve(__dirname, "index.html"),
+      // VERY IMPORTANT: force ONLY your index.html as entry
+      input: path.resolve(__dirname, 'index.html'),
+      external: ['@linera/client'],
     },
+  },
+  optimizeDeps: {
+    exclude: ['@linera/client'],
   },
   esbuild: {
     supported: {
-      "top-level-await": true,
+      'top-level-await': true,
     },
   },
   plugins: [react()],
-});
+})
