de979b2d9cfd87fb7734fd01afe9e5a8d5f5b6cf
Girum <70520544+girume1@users.noreply.github.com>
2026-01-23 21:12:21 +0300

    Delete frontend directory

--- frontend/src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const LINERA_RPC_URL = "https://faucet.testnet-conway.linera.net/";
-
-// YOUR ACTUAL APPLICATION ID
-export const GAME_APP_ID = "71cb9bd008abd8745f658d933ee449d84ccc4c40966d270d94da35147be3fbac";
-
-// YOUR ACTUAL CHAIN ID  
-export const CHAIN_ID = "74a81927acd5c0048a05bdb206ce6252b29a4d3c423fbcfae8e32d0a30c3d88f";
